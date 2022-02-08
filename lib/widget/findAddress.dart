725d19f2423c393d835ed3530b49c4fb33977318
SeungyoungLee <kisun66@naver.com>
2022-02-08 11:05:58 +0900

    1. 메인페이지 버튼 하단 버튼설명 추가

--- lib/widget/findAddress.dart
+++ lib/widget/findAddress.dart
@@ -18,43 +18,50 @@ class _findAdressState extends State<FindAddress> {
 
   @override
   Widget build(BuildContext context) {
-    return IconButton(
-      padding: EdgeInsets.all(0.0), // 기본 padding 제거
-      icon: Icon(Icons.book, size: 50), // 50 이상으로 설정 시 오른쪽으로 넘어감
-      onPressed: () async {
-        Kpostal kPostal = await Navigator.push(context, MaterialPageRoute(builder: (_) => KpostalView(
-            kakaoKey: '53a136cf20bd4263c00b68fbf310d71f',
-            onLoading: loading.spinningCircle('주소창을 불러오고있습니다...'),
-        )));
-        if(kPostal.postCode.isNotEmpty){
-          try{
-            kakaoLatitude = kPostal.kakaoLatitude.toString();
-            kakaoLongitude = kPostal.kakaoLongitude.toString();
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        IconButton(
+          padding: EdgeInsets.all(0.0), // 기본 padding 제거
+          icon: Icon(Icons.book, size: 50), // 50 이상으로 설정 시 오른쪽으로 넘어감
+          onPressed: () async {
+            Kpostal kPostal = await Navigator.push(context, MaterialPageRoute(builder: (_) => KpostalView(
+                kakaoKey: '53a136cf20bd4263c00b68fbf310d71f',
+                onLoading: loading.spinningCircle('주소창을 불러오고있습니다...'),
+            )));
+            if(kPostal.postCode.isNotEmpty){
+              try{
+                kakaoLatitude = kPostal.kakaoLatitude.toString();
+                kakaoLongitude = kPostal.kakaoLongitude.toString();
 
-            await Navigator.push(
-                context,
-                PageRouteBuilder(
-                  pageBuilder: (c, a1, a2) => kakaoMap.KakaoMap(kakaoLatitude : kakaoLatitude, kakaoLongitude : kakaoLongitude),
-                  transitionsBuilder: (context, animation, secondaryAnimation, child) {
-                    const begin = Offset(0.0, 1.0);
-                    const end = Offset.zero;
-                    const curve = Curves.ease;
+                await Navigator.push(
+                    context,
+                    PageRouteBuilder(
+                      pageBuilder: (c, a1, a2) => kakaoMap.KakaoMap(kakaoLatitude : kakaoLatitude, kakaoLongitude : kakaoLongitude),
+                      transitionsBuilder: (context, animation, secondaryAnimation, child) {
+                        const begin = Offset(0.0, 1.0);
+                        const end = Offset.zero;
+                        const curve = Curves.ease;
 
-                    var tween = Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
+                        var tween = Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
 
-                    return SlideTransition(
-                      position: animation.drive(tween),
-                      child: child,
-                    );
-                  },
-                  transitionDuration: Duration(milliseconds: 200),
-                )
-            );
-          } catch(e) {
-            Text('문제가 발생했습니다.');
-          }
-        }
-      },
+                        return SlideTransition(
+                          position: animation.drive(tween),
+                          child: child,
+                        );
+                      },
+                      transitionDuration: Duration(milliseconds: 200),
+                    )
+                );
+              } catch(e) {
+                Text('문제가 발생했습니다.');
+              }
+            }
+          },
+        ),
+        SizedBox(height: 10),
+        Text('주소로 검색')
+      ]
     );
   }
 }
--- lib/widget/findMyGps.dart
+++ lib/widget/findMyGps.dart
@@ -14,43 +14,50 @@ class FindMyGps extends StatefulWidget {
 class _FindMyGpsState extends State<FindMyGps> {
   @override
   Widget build(BuildContext context) {
-    return IconButton(
-      padding: EdgeInsets.all(0),
-      icon: Icon(Icons.gps_fixed, size: 50),
-      onPressed: () async {
-        Navigator.push(
-            context,
-            PageRouteBuilder(
-              pageBuilder: (c, a1, a2) => FutureBuilder(
-                  future: getCurrentLocation(),
-                  builder: (BuildContext context, AsyncSnapshot snapshot){
-                    if(snapshot.hasData == false) {
-                      return loading.spinningCircle('GPS정보를 수신중입니다...');
-                    } else if (snapshot.hasError) {
-                      return Text('Error');
-                    } else {
-                      return kakaoMap.KakaoMap(
-                        kakaoLatitude: snapshot.data.latitude.toString(),
-                        kakaoLongitude: snapshot.data.longitude.toString(),
-                      );
-                    }
-                  }
-              ),
-              transitionsBuilder: (context, animation, secondaryAnimation, child) {
-                const begin = Offset(0.0, 1.0);
-                const end = Offset.zero;
-                const curve = Curves.ease;
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        IconButton(
+          padding: EdgeInsets.all(0),
+          icon: Icon(Icons.gps_fixed, size: 50),
+          onPressed: () async {
+            Navigator.push(
+                context,
+                PageRouteBuilder(
+                  pageBuilder: (c, a1, a2) => FutureBuilder(
+                      future: getCurrentLocation(),
+                      builder: (BuildContext context, AsyncSnapshot snapshot){
+                        if(snapshot.hasData == false) {
+                          return loading.spinningCircle('GPS정보를 수신중입니다...');
+                        } else if (snapshot.hasError) {
+                          return Text('Error');
+                        } else {
+                          return kakaoMap.KakaoMap(
+                            kakaoLatitude: snapshot.data.latitude.toString(),
+                            kakaoLongitude: snapshot.data.longitude.toString(),
+                          );
+                        }
+                      }
+                  ),
+                  transitionsBuilder: (context, animation, secondaryAnimation, child) {
+                    const begin = Offset(0.0, 1.0);
+                    const end = Offset.zero;
+                    const curve = Curves.ease;
 
-                var tween = Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
+                    var tween = Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
 
-                return SlideTransition(
-                  position: animation.drive(tween),
-                  child: child,
-                );
-              },
-            )
-        );
-      }
+                    return SlideTransition(
+                      position: animation.drive(tween),
+                      child: child,
+                    );
+                  },
+                )
+            );
+          }
+        ),
+        SizedBox(height: 10),
+        Text('현재위치로 검색')
+      ],
     );
   }
 }
